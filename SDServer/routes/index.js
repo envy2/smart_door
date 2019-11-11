4288eb12dade3420fcbf3383f6d8d89a9f1d5794
김경태 <kkt3526@naver.com>
2019-11-11 09:08:13 +0900

    Smart Door Mobile Back

---
+++ SDServer/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var Resident = require('../models/Resident');
+
+router.post('/', function (req, res) {
+    var resident = new Resident();
+    resident.name = req.body.name;
+    resident.age = req.body.age;
+    
+    resident.save(function (err) {
+        if (err) {
+            return res.json({ result: false })
+        }
+        res.json({ result: true });
+    });
+});
+
+
+
+module.exports = router;
